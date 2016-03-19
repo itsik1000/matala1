80ba07aca922e85980683bfd6a44c16329be3c99
איציק <@192.168.10.233>
2016-03-19 20:11:18 +0200

    all the functions.

---
+++ matala1/src/matala1/Graph_algo.java
@@ -0,0 +1,21 @@
+package matala1;
+
+import java.util.Vector;
+
+public class Graph_algo {
+	public double distance(int start, int end)
+	{
+		return Double.NEGATIVE_INFINITY;
+	}
+	public Vector<Integer> thePath(int start, int end)
+	{
+		return null;
+	}
+	public double distance(int start, int end, Vector<Integer> blackList)
+	{
+		
+		
+		return Double.NEGATIVE_INFINITY;
+	}
+
+}
